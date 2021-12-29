1bfed70c4429fd321bca2d9f86616326dc4b9cad
CrisGadea <cristianhernangadea@gmail.com>
2021-12-29 13:08:30 -0300

    changed card's colors

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,9 +1,49 @@
-import React from "react";
+import {React, useState, useEffect} from "react";
 import HabilidadesPokemon from "./HabilidadesPokemon";
 
 const PokeCard = ({ imagen, nombre, habilidades, peso, altura, tipo, experiencia}) => {
+    //let card = "";
+    
+    const [card, setCard] = useState("");
+
+    useEffect(() => {
+        switch (tipo) {
+            case "grass":
+                setCard("card card--grass");
+                
+                break;
+            case "ice":
+                setCard("card card--ice");
+                break;
+            case "fire":
+                setCard("card card--fire");
+            break;
+            case "electric":
+                setCard("card card--electric");
+            break;
+            case "fairy":
+                setCard("card card--fairy");
+            break;
+            case "dark":
+                setCard("card card--dark");
+            break;
+            case "psychic":
+                setCard("card card--psychic");
+            break;
+            case "water":
+                setCard("card card--water");
+            break;
+            case "normal":
+                setCard("card card--normal");
+            break;
+        
+            default:
+                break;
+        }
+    }, [tipo])
+    
     return <div id="cards">
-            <figure className="card card--normal">
+            <figure className={card}>
                 <div className="card__image-container">
                     <img src={imagen} alt="tipo" className="card__image"/>   
                 </div>
